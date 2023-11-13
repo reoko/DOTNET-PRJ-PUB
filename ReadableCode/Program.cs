6df3a44de3fc189baa8828785fd884052ccd4ba5
reoko <johnmompil@gmail.com>
2023-11-13 13:30:53 +0300

    update ReadableCode/Program.cs add sample code to improve code readability

--- ReadableCode/Program.cs
+++ ReadableCode/Program.cs
@@ -4,6 +4,8 @@
   consist of a letter from A to E, and a three
   digit number. Ex. A123.
 */
+
+/*
 Random random = new Random();
 string[] orderIDs = new string[5];
 
@@ -20,3 +22,18 @@ foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
 }
+*/
+
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
