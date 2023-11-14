1bc0bf031e9a9065c0dd15b89e8ad3bbf4540ebd
reoko <johnmompil@gmail.com>
2023-11-14 15:20:00 +0300

    update project the code for Project 1 is complete

--- DotNetConsoleApp_Do_While/Program.cs
+++ DotNetConsoleApp_Do_While/Program.cs
@@ -17,6 +17,7 @@ do{
 The Boolean expression evaluated by the while statement can be used to ensure input meets a specified requirement. For example, if a prompt asks the user to enter a string that includes at least three characters, the following code could be used:
 */
 
+/*
 string? readResult;
 bool validEntry = false;
 Console.WriteLine("Enter a string containing at least three characters:");
@@ -34,4 +35,53 @@ do
             Console.WriteLine("Your input is invalid, please try again.");
         }
     }
-} while (validEntry == false);
+}while(validEntry == false);
+*/
+
+/*
+If need to convert string value from Console.ReadLine() input to numeric type use int.TryParse()
+*/
+/*
+int numericValue = 0;
+bool validNumber = false;
+
+validNumber = int.TryParse(readResult, out numericValue);
+
+Console.WriteLine($"validNumber: {validNumber}; readResult: {readResult}; numericValue: {numericValue}");
+*/
+
+/*
+Code Project 1 - write code that validates integer input
+*/
+
+string? strReadResult;
+bool bolValidEntry = false;
+int intValidInteger = 0;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do{
+    strReadResult = Console.ReadLine();
+    if(strReadResult != null)
+    {
+        bolValidEntry = int.TryParse(strReadResult, out intValidInteger);
+
+        if ((intValidInteger < 5 || intValidInteger > 10) && bolValidEntry == true) 
+        {
+            Console.WriteLine($"You entered {intValidInteger}. Please enter a number between 5 and 10.");
+            bolValidEntry = false;
+        }
+        else if (intValidInteger >= 5 && intValidInteger <= 10 && bolValidEntry == true) 
+        { 
+            Console.WriteLine($"Your input value ({intValidInteger}) has been accepted."); 
+            bolValidEntry = true; 
+            break; 
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you entered an invalid number, please try again");
+            bolValidEntry = false;
+        }
+    }
+}while(bolValidEntry == false);
+
