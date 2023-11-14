9bc91e8794acf700c34af4d949bc3d9eea4ba67f
reoko <johnmompil@gmail.com>
2023-11-14 14:25:44 +0300

    update project the code demonstrate the control of do while loop.

--- DotNetConsoleApp_Do_While/Program.cs
+++ DotNetConsoleApp_Do_While/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Manage user input during this Challenge
+*/
+
+/*
+string? readResult;
+
+Console.WriteLine("Enter a string:");
+
+do{
+    readResult = Console.ReadLine();
+}while(readResult == null);
+*/
+
+
+/*
+The Boolean expression evaluated by the while statement can be used to ensure input meets a specified requirement. For example, if a prompt asks the user to enter a string that includes at least three characters, the following code could be used:
+*/
+
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
